fd4cc129be246f53e14fd0dc19d2c5852b4e7745
Rémi Jarjat <dreeckan@gmail.com>
2021-04-21 14:21:48 +0200

    Exercice 2

--- 2-loops/2-loops.js
+++ 2-loops/2-loops.js
@@ -7,3 +7,44 @@ let shoes = [
     'Poseidon étanches',
     'Hadès méga-dark',
 ];
+
+// Boucle for
+for (let i = 0; i < shoes.length; i++) {
+    console.log('Element ' + i + ' : ' + shoes[i]);
+}
+
+// Boucle for...of
+let i = 0;
+for (const shoe of shoes) {
+    console.log('Element ' + i + ' : ' + shoe);
+    i++;
+}
+
+// Boucle while
+let j = 0;
+while (j < shoes.length) {
+    console.log('Element ' + j + ' : ' + shoes[j]);
+    j++;
+}
+
+// Boucle forEach
+shoes.forEach(function (shoe, index) {
+    console.log('Element ' + index + ' : ' + shoe);
+});
+
+
+// Méthode alternative, avec indexOf()
+//
+// let possibleValues = [0, 1, 2, 3, 4];
+// let userInput = null;
+//
+// while (isNaN(userInput) || possibleValues.indexOf(parseInt(userInput)) < 0) {
+//     userInput = prompt('Écrivez un nombre entre 0 et 4 (inclus)');
+// }
+// console.log(shoes[userInput]);
+
+let userInput = -1;
+while (isNaN(userInput) || userInput < 0 || userInput > 4) {
+    userInput = prompt('Écrivez un nombre entre 0 et 4 (inclus)');
+}
+console.log(shoes[userInput]);
